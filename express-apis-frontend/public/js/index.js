d5dbcb57dc85c04485594a15bf06f2941ea84f02
Peter Joh <johpeter@gmail.com>
2021-08-18 14:43:50 -0600

    test

--- express-apis-frontend/public/js/index.js
+++ express-apis-frontend/public/js/index.js
@@ -1,1 +1,11 @@
-console.log("Hello from index.js!");
+// console.log("Hello from index.js!");
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+      const res = await fetch("http://localhost:8080/tweets");
+      const { tweets } = await res.json();
+      console.log(tweets);
+    } catch (e) {
+      console.error(e);
+    }
+  });
